04034b596a08d75676ddb907cea0680c44142a35
Gulshan <gulshan190210@gmail.com>
2022-06-27 14:25:04 +0530

    setup of socket.io

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+io.on("connection", ()=>
+{
+    console.log("New websocket Connection!");
+});
+
 app.get("/", function(req, res)
 {
-    res.send("Hello World!");
+    res.render("chat");
 });
-app.listen(3000, function(err)
+
+server.listen(3000, function(err)
 {
     if(err)
     {
